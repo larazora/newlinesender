7c80b68a8df528c1ab40754cc82d312d079625c3
larazora <85832705+larazora@users.noreply.github.com>
2021-09-13 17:07:02 +0900

    good commit2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT linesender:app
+worker: python linesender.py
--- linesender.py
+++ linesender.py
@@ -36,8 +36,8 @@ def menu_get():
 def main():
     schedule.every(1).day.at("06:13").do(menu_get)
     schedule.every(1).day.at("11:26").do(menu_get)
-    schedule.every(1).day.at("16:43").do(menu_get)
-    schedule.every(1).day.at("16:45").do(menu_get)
+    schedule.every(1).day.at("17:10").do(menu_get)
+    schedule.every(1).day.at("17:09").do(menu_get)
     print("start!")
 
     while True:
