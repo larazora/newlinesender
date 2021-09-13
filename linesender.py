54ac99c21610c0234b2c1178492ba86215477c20
larazora <85832705+larazora@users.noreply.github.com>
2021-09-13 17:18:06 +0900

    commit2

--- linesender.py
+++ linesender.py
@@ -36,8 +36,8 @@ def menu_get():
 def main():
     schedule.every(1).day.at("06:13").do(menu_get)
     schedule.every(1).day.at("11:26").do(menu_get)
-    schedule.every(1).day.at("17:14").do(menu_get)
-    schedule.every(1).day.at("17:15").do(menu_get)
+    schedule.every(1).day.at("17:40").do(menu_get)
+    schedule.every(1).day.at("17:20").do(menu_get)
     print("start!")
 
     while True:
